ec825cee14266a46a6d07f037348cee475c1eb82
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-11 00:04:20 +0300

    feat: Add the getters, fix the style a little

--- src/utilities.js
+++ src/utilities.js
@@ -1,17 +1,27 @@
 /**
- * Represents common data that a single product holds, such as
- * its price, energy value and other similar characteristics.
+ * Represents common data that a single product holds, such
+ * as its price, energy and other similar characteristics.
  */
 class ProductData {
 	#energy;
 	#price;
 
 	/**
-	 * @param {number} energy The energy value, in calories.
+	 * @param {number} energy The energy, in calories.
 	 * @param {number} price The price, in tugriks.
 	 */
 	constructor(energy, price) {
 		this.#energy = energy;
 		this.#price = price;
+	}
+
+	/** @return {number} */
+	get energy() {
+		return this.#energy;
+	}
+
+	/** @return {number} */
+	get price() {
+		return this.#price;
 	}
 }
