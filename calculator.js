30a059daed53f939102877dc947a285ff7996114
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-12 08:51:55 +0300

    feat: Add the examples, make some properties return as string literals

--- calculator.js
+++ calculator.js
@@ -7,10 +7,12 @@ class Properties {
   /**
    * @param {Object} props        The list of properties.
    * @param {number} props.energy The energy value, in calories.
+   * @param {string} props.id     The unique identificator
    * @param {number} props.price  The price, in tugriks.
    */
   constructor(props = {}) {
     this._energy = props.energy || 0;
+    this._id     = props.id     || "";
     this._price  = props.price  || 0;
   }
 
@@ -24,6 +26,11 @@ class Properties {
     return this._energy;
   }
 
+  /** @return {string} The unique identificator. */
+  get id() {
+    return this._id;
+  }
+
   /** @return {number} The price, in tugriks. */
   get price() {
     return this._price;
@@ -35,7 +42,12 @@ class Properties {
    * @return {Properties} This instance (for chaining).
    */
   add(props) {
-    Object.keys(this).forEach((key) => this[key] += props[key]);
+    Object.keys(this).forEach((key) => {
+      if (key !== "id") {
+        this[key] += props[key];
+      }
+    });
+
     return this;
   }
 
@@ -45,7 +57,12 @@ class Properties {
    * @return {Properties} This instance (for chaining).
    */
   delete(props) {
-    Object.keys(this).forEach((key) => this[key] -= props[key]);
+    Object.keys(this).forEach((key) => {
+      if (key !== "id") {
+        this[key] -= props[key];
+      }
+    });
+
     return this;
   }
 
@@ -55,7 +72,12 @@ class Properties {
    * @return {Properties} This instance (for chaining).
    */
   multiply(value) {
-    Object.keys(this).forEach((key) => this[key] *= value);
+    Object.keys(this).forEach((key) => {
+      if (key !== "id") {
+        this[key] *= value;
+      }
+    });
+
     return this;
   }
 }
@@ -72,7 +94,7 @@ class Product {
    * @const {Error} The error to throw when no product
    * filling is provided, or the provided filling is not valid.
    */
-  static _FILL_FAIL_ERROR;
+  static _FILLING_FAIL_ERROR;
 
   /**
    * @const {Error} The error to throw when no product
@@ -91,16 +113,16 @@ class Product {
 
   constructor() {
     if (!this.constructor._ERRORS_INITED) {
-      this.constructor._FILL_FAIL_ERROR = new Error(
-        `the ${this.constructor._NAME} filling is not chosen or is invalid`
+      this.constructor._FILLING_FAIL_ERROR = new Error(
+        `the ${this.constructor._NAME} filling is not chosen or is unknown`
       );
 
       this.constructor._SIZE_FAIL_ERROR = new Error(
-        `the ${this.constructor._NAME} size is not chosen or is invalid`
+        `the ${this.constructor._NAME} size is not chosen or is unknown`
       );
 
       this.constructor._TYPE_FAIL_ERROR = new Error(
-        `the ${this.constructor._NAME} type is not chosen or is invalid`
+        `the ${this.constructor._NAME} type is not chosen or is unknown`
       );
     } else {
       this.constructor._ERRORS_INITED = true;
@@ -157,31 +179,62 @@ class Product {
   }
 }
 
-/** Represents a burger, nothing more to explain here. */
+/**
+ * Represents a burger, nothing more to explain here.
+ * <code>
+ *   <pre>
+ *     // A big burger with cheese.
+ *     const burger1 = new Burger(Burger.BIG, Burger.WITH_CHEESE);
+ *     // A small burger with potato.
+ *     const burger2 = new Burger(Burger.SMALL, Burger.WITH_POTATO);
+ *     // A small burger with salad.
+ *     const burger3 = new Burger(Burger.SMALL, Burger.WITH_SALAD);
+ *     // Error, unknown burger size provided.
+ *     const burger4 = new Burger('average');
+ *     // Error, no burger size provided.
+ *     const burger5 = new Burger();
+ *     // Error, unknown burger filling provided.
+ *     const burger6 = new Burger(Burger.SMALL, 'fish');
+ *     // Error, no burger filling provided.
+ *     const burger7 = new Burger(Burger.SMALL);
+ *     // Get the burger energy value.
+ *     console.log(burger1.energy); // 60
+ *     // Get the burger price.
+ *     console.log(burger1.price); // 110
+ *     // Get the burger size.
+ *     console.log(burger1.size); // "big"
+ *     // Get the burger filling.
+ *     console.log(burger1.filling); // "cheese"
+ *   </pre>
+ * </code>
+ */
 class Burger extends Product {
   /** @const {string} The name which identifies the product. */
   static _NAME = "burger";
 
   /** @const {Properties} The big burger. */
-  static BIG = new Properties({ energy: 40, price: 100 });
+  static BIG = new Properties({ id: "big", energy: 40, price: 100 });
 
   /** @const {Properties} The small burger. */
-  static SMALL = new Properties({ energy: 20, price: 50 });
+  static SMALL = new Properties({ id: "small", energy: 20, price: 50 });
 
   /** @const {Properties} The burger with cheese. */
-  static WITH_CHEESE = new Properties({ energy: 20, price: 10 });
+  static WITH_CHEESE =
+    new Properties({ id: "cheese", energy: 20, price: 10 });
 
   /** @const {Properties} The burger with potato. */
-  static WITH_POTATO = new Properties({ energy: 10, price: 15 });
+  static WITH_POTATO =
+    new Properties({ id: "potato", energy: 10, price: 15 });
 
   /** @const {Properties} The burger with salad. */
-  static WITH_SALAD = new Properties({ energy: 5, price: 20 });
+  static WITH_SALAD =
+    new Properties({ id: "salad", energy: 5, price: 20 });
 
   /**
    * @param {Properties} size The size of a burger, from a set.
-   * @param {Properties} fill The fillfor a burger, from a set.
+   * @param {Properties} filling The filling of a burger, from a set.
    */
-  constructor(size, fill) {
+  constructor(size, filling) {
     super();
 
     if (size instanceof Properties) {
@@ -190,34 +243,54 @@ class Burger extends Product {
       throw this.constructor._SIZE_FAIL_ERROR;
     }
 
-    if (fill instanceof Properties) {
-      this._props.add(this._fill = fill);
+    if (filling instanceof Properties) {
+      this._props.add(this._filling = filling);
     } else {
-      throw this.constructor._FILL_FAIL_ERROR;
+      throw this.constructor._FILLING_FAIL_ERROR;
     }
   }
 
-  /** @return {string} The fill of the burger, from a set. */
-  get fill() {
-    return this._fill;
+  /** @return {string} The filling of the burger, from a set. */
+  get filling() {
+    return this._filling.id;
   }
 
   /** @return {string} The size of the burger, from a set. */
   get size() {
-    return this._size;
+    return this._size.id;
   }
 }
 
-/** Represents a drink, such as a cup of coffee or a glass of cola. */
+/**
+ * Represents a drink, such as a cup of coffee or a glass of cola.
+ * <code>
+ *   <pre>
+ *     // A cup of coffee.
+ *     const drink1 = new Drink(Drink.COFFEE);
+ *     // A glass of cola.
+ *     const drink2 = new Drink(Drink.COLA);
+ *     // Error, unknown drink provided.
+ *     const drink3 = new Drink('tea');
+ *     // Error, no drink provided.
+ *     const drink4 = new Drink();
+ *     // Get the drink energy value.
+ *     console.log(drink1.energy); // 20
+ *     // Get the drink price.
+ *     console.log(drink1.price); // 80
+ *     // Get the drink type.
+ *     console.log(drink1.type); // "coffee"
+ *   </pre>
+ * </code>
+ */
 class Drink extends Product {
   /** @const {string} The name which identifies the product. */
   static _NAME = "drink";
 
   /** @const {Properties} The coffee. */
-  static COFFEE = new Properties({ energy: 20, price: 80 });
+  static COFFEE = new Properties({ id: "coffee", energy: 20, price: 80 });
 
   /** @const {Properties} The cola. */
-  static COLA = new Properties({ energy: 40, price: 50 });
+  static COLA = new Properties({ id: "cola", energy: 40, price: 50 });
 
   /** @param {Properties} type The type of a drink, from a set. */
   constructor(type) {
@@ -232,11 +305,40 @@ class Drink extends Product {
 
   /** @return {string} The type of the drink, from a set. */
   get type() {
-    return this._type;
+    return this._type.id;
   }
 }
 
-/** Represents a salad, such as a Caesar salad or an Olivier salad. */
+/**
+ * Represents a salad, such as a Caesar salad or an Olivier salad.
+ * <code>
+ *   <pre>
+ *     // A Caesar salad, 100 grams.
+ *     const salad1 = new Salad(Salad.CAESAR);
+ *     // A Caesar salad, 156 grams.
+ *     const salad2 = new Salad(Salad.CAESAR, 156);
+ *     // An Olivier salad, 240 grams.
+ *     const salad3 = new Salad(Salad.OLIVIER, 240);
+ *     // Error, invalid salad weight provided.
+ *     const salad4 = new Salad(Salad.OLIVIER, -3);
+ *     // Error, unknown salad provided.
+ *     const salad5 = new Salad('crab');
+ *     // Error, no salad provided.
+ *     const salad6 = new Salad();
+ *     // Get the salad energy value.
+ *     console.log(salad1.energy); // 20
+ *     // Get the salad price.
+ *     console.log(salad1.price); // 100
+ *     // Get the salad type.
+ *     console.log(salad1.type); // "caesar"
+ *     // Get the salad weight.
+ *     console.log(salad1.weight); // 100
+ *     // Update the salad weight.
+ *     salad1.weight = 128;
+ *     console.log(salad1.weight); // 128
+ *   </pre>
+ * </code>
+ */
 class Salad extends Product {
   /** @const {string} The name which identifies the product. */
   static _NAME = "salad";
@@ -248,10 +350,10 @@ class Salad extends Product {
   static _WEIGHT_WARN = "the salad weight is invalid, assuming 100g";
 
   /** @const {Properties} The Caesar salad. */
-  static CAESAR = new Properties({ energy: 20, price: 100 });
+  static CAESAR = new Properties({ id: "caesar", energy: 20, price: 100 });
 
   /** @const {Properties} The Olivier salad. */
-  static OLIVIER = new Properties({ energy: 80, price: 50 });
+  static OLIVIER = new Properties({ id: "olivier", energy: 80, price: 50 });
 
   /** @var {number} The weight of the salad. */
   _weight = 100;
@@ -274,8 +376,10 @@ class Salad extends Product {
         console.log(this.constructor._WEIGHT_LOG);
         break;
       case "number":
-        this.weight = weight;
-        break;
+        if (weight >= 0) {
+          this.weight = weight;
+          break;
+        }
       default:
         console.warn(this.constructor._WEIGHT_WARN);
     }
@@ -283,7 +387,7 @@ class Salad extends Product {
 
   /** @return {string} The type of the salad, from a set. */
   get type() {
-    return this._type;
+    return this._type.id;
   }
 
   /** @return {number} The weight of a salad, in grams. */
